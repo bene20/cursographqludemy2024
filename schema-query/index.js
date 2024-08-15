86c47c3b07848a791eba06b5b17f6870cfbf9c3a
Ebenézer Rangel Botelho <bene20@gmail.com>
2024-08-15 17:13:47 -0300

    Até aula 15

---
+++ schema-query/index.js
@@ -0,0 +1,24 @@
+const {ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+  #Pontos de entrada da API
+  type Query {
+    ola: String
+    horaAtual: String
+  }
+`
+
+const resolvers ={
+    Query: {
+        ola() {
+            return 'Bom dia!'
+        },
+        horaAtual() {
+            return `${new Date()}`
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs,resolvers})
+
+server.listen().then(({url}) => {console.log(`Executando em ${url}`)})
